bc18eda19c775e75dda7a6902330f2d61e7eb841
Benjamin Winchester <benjamin.m.winchester@googlemail.com>
2020-03-29 23:07:13 -0400

    Fixed conditional rendering of video template

--- src/templates/videoTemplate.js
+++ src/templates/videoTemplate.js
@@ -3,29 +3,40 @@ import { graphql, Link } from "gatsby"
 import Img from "gatsby-image"
 import SEO from "../components/seo"
 import Layout from "../components/layout"
+import { useAuth } from 'react-use-auth'
 
 export default ({ data }) => {
 
   const video = data.s3Object
+  const { isAuthenticated, user } = useAuth()
 
   let title = video.Key.split(".")[0].split("_").join(" ")
   let filePath = `https://sophiesiteassets.s3.us-east-2.amazonaws.com/${video.Key}`
 
+  if(isAuthenticated() && Object.keys(access).includes(user.email) && access[user.email].includes("YogaClass001")){
+    return (
+      <Layout>
+      <SEO title={title} />
+        <div className="blogCard">
+          <div className="blogCardData">
+            <h1 className="pageTitle">{title}</h1>
+            <video className="videoEmbed" controls>
+              <source src={filePath} type="video/mp4" />
+              Your browser does not support the video tag.
+            </video>
+            <Link to="/" className="App-Link">Home</Link>
+          </div>
+        </div>
+        </ Layout>
+    )
+  } else {
   return (
     <Layout>
-    <SEO title={title} />
-      <div className="blogCard">
-        <div className="blogCardData">
-          <h1 className="pageTitle">{title}</h1>
-          <video className="videoEmbed" controls>
-            <source src={filePath} type="video/mp4" />
-            Your browser does not support the video tag.
-          </video>
-          <Link to="/" className="App-Link">Home</Link>
-        </div>
-      </div>
-      </ Layout>
-  )
+      <SEO title={title} />
+      <span className="pageTitle">You do not have authorization for this site</span>
+    </Layout>
+  ) 
+}
 }
 
 export const query = graphql`
